207ec6e07886d3742c8e78853f457dcaa8e61bcb
John <johneliud4@gmail.com>
2026-03-28 10:23:26 +0300

    feat(frontend): add movie-detail component placeholder.

---
+++ src/app/features/movies/movie-detail/movie-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-detail',
+  standalone: true,
+  template: `<p>Movie detail page</p>`,
+})
+export class MovieDetailComponent {}
